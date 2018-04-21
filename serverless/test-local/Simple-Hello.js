674af399b041e3b135b89e32302a04cfaae4eadf
손성수 <sungsu7437@naver.com>
2018-04-21 21:05:07 +0900

    Update Simple-Hello.js

--- serverless/test-local/Simple-Hello.js
+++ serverless/test-local/Simple-Hello.js
@@ -11,10 +11,8 @@ exports.handler = function(event, context, callback) {
     console.log('logGroupName =', context.logGroupName);
     console.log('logStreamName =', context.logStreamName);
     console.log('clientContext =', context.clientContext);
-    console.log('clientContext =', context.clientContext);  
-    if (typeof event.name === 'undefined') {
-         callback("There is no key named name",null);
-    }
+    console.log('clientContext =', context.clientContext); 
+    
     callback(null, "Hello " + event.name); 
     console.log('end time =', context.getRemainingTimeInMillis());
 };
