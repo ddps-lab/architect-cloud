c8a0b764b6e6df33ca727c338b814ac435579ff8
sungsu7437 <sungsu7437@naver.com>
2018-04-21 19:43:19 +0900

    test-consol added

---
+++ serverless/test-local/test.js
@@ -0,0 +1,23 @@
+var path = require("path");
+
+const lambdaLocal = require('lambda-local');
+ 
+var jsonPayload = {
+    'name2': 'name',
+}
+ 
+lambdaLocal.execute({
+    event: jsonPayload,
+    lambdaPath: path.join(__dirname, '../Simple-Hello.js'),
+    profilePath: '~/.aws/credentials',
+    profileName: 'default',
+    timeoutMs: 3000,
+    callback: function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+        }
+    }
+});
+
