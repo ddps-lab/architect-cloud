727a1304f0112492f06f72191e9633abed3692af
Kyungyong Lee <leeky@kookmin.ac.kr>
2018-04-22 10:58:05 +0900

    removing unnessary spaces

--- serverless/simple-hello-cli/simple-hello-cli.js
+++ serverless/simple-hello-cli/simple-hello-cli.js
@@ -7,13 +7,13 @@ exports.handler = function(event, context, callback) {
     console.log('functionName =', context.functionName);
     console.log('functionVersion =', context.functionVersion);
     console.log('AWSrequestID =', context.awsRequestId);
-    console.log( 'memoryLimitinMB =', context.awsRequestId);
+    console.log('memoryLimitinMB =', context.awsRequestId);
     console.log('logGroupName =', context.logGroupName);
     console.log('logStreamName =', context.logStreamName);
     console.log('clientContext =', context.clientContext);
     console.log('clientContext =', context.clientContext);  
     if (typeof event.name === 'undefined') {
-         callback("There is no key named name",null);
+         callback("There is no key named name", null);
     }
     callback(null, "Hello " + event.name);
     console.log('end time =', context.getRemainingTimeInMillis());
