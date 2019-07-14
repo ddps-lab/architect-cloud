c6e98ecb303d319d0596a84e80e4ce9edfb1905e
KimJungChul <kjc5443@gmail.com>
2019-07-14 14:04:07 +0900

    adding lambda snap_and_tag

---
+++ serverless/snap_and_tag/lambda_function.py
@@ -0,0 +1,37 @@
+# Snap_and_Tag Lambda function
+#
+# This function is triggered when Auto Scaling launches a new instance.
+# A snapshot of EBS volumes will be created and a tag will be added.
+
+from __future__ import print_function
+
+import json, boto3
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    # Extract the EC2 instance ID from the Auto Scaling event notification
+    message = event['Records'][0]['Sns']['Message']
+    autoscalingInfo = json.loads(message)
+    ec2InstanceId = autoscalingInfo['EC2InstanceId']
+
+    # Snapshot all EBS volumes attached to the instance
+    ec2 = boto3.resource('ec2')
+    
+    for v in ec2.volumes.filter(Filters=[{'Name': 'attachment.instance-id', 'Values': [ec2InstanceId]}]):
+        description = 'Autosnap-%s-%s' % ( ec2InstanceId, v.volume_id )
+
+        if v.create_snapshot(Description = description):
+            print("\t\tSnapshot created with description [%s]" % description)
+
+    # Add a tag to the EC2 instance: Key = Snapshots, Value = Created
+    ec2 = boto3.client('ec2')
+    response = ec2.create_tags(
+        Resources=[ec2InstanceId],
+        Tags=[{'Key': 'Snapshots', 'Value': 'Created'}]
+    )
+    print ("***Tag added to EC2 instance with id: " + ec2InstanceId)
+
+    # Finished!
+    return ec2InstanceId
