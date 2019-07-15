1e0658c0e33acf529f981441b07c25af98a06b5f
KimJungChul <kjc5443@gmail.com>
2019-07-15 16:30:08 +0900

    adding total_notifier

---
+++ serverless/total_notifier/lambda_function.py
@@ -0,0 +1,62 @@
+# TotalNotifier Lambda function
+#
+# This function is triggered when values are inserted into the Transactions DynamoDB table.
+# Transaction totals are calculated and notifications are sent to SNS if limits are exceeded.
+
+from __future__ import print_function
+import json, boto3
+
+# Connect to SNS
+sns = boto3.client('sns')
+alertTopic = 'HighBalanceAlert'
+snsTopicArn = [t['TopicArn'] for t in sns.list_topics()['Topics'] if t['TopicArn'].endswith(':' + alertTopic)][0]
+
+# Connect to DynamoDB
+dynamodb = boto3.resource('dynamodb')
+transactionTotalTableName = 'TransactionTotal'
+transactionsTotalTable = dynamodb.Table(transactionTotalTableName);
+
+# This handler is executed every time the Lambda function is triggered
+def lambda_handler(event, context):
+
+    # Show the incoming event in the debug log
+    print("Event received by Lambda function: " + json.dumps(event, indent=2))
+
+    # For each transaction added, calculate the new Transactions Total
+    for record in event['Records']:
+        customerId = record['dynamodb']['NewImage']['CustomerId']['S']
+        transactionAmount = int(record['dynamodb']['NewImage']['TransactionAmount']['N'])
+
+        # Update the customer's total in the TransactionTotal DynamoDB table
+        response = transactionsTotalTable.update_item(
+            Key={
+                'CustomerId': customerId
+            },
+            UpdateExpression="add accountBalance :val",
+            ExpressionAttributeValues={
+                ':val': transactionAmount
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        # Retrieve the latest account balance
+        latestAccountBalance = response['Attributes']['accountBalance']
+        print("Latest account balance: " + format(latestAccountBalance))
+
+        # If balance > $1500, send a message to SNS
+        if latestAccountBalance >= 1500:
+
+            # Construct message to be sent
+            message = '{"customerID": "' + customerId + '", ' + '"accountBalance": "' + str(latestAccountBalance) + '"}'
+            print(message)
+
+            # Send message to SNS
+            sns.publish(
+                TopicArn=snsTopicArn,
+                Message=message,
+                Subject='Warning! Account balance is very high',
+                MessageStructure='raw'
+            )
+
+    # Finished!
+    return 'Successfully processed {} records.'.format(len(event['Records']))
