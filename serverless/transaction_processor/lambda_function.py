4a16422664e053ecf5dcb96a5ff599f968faa2c6
KimJungChul <kjc5443@gmail.com>
2019-07-17 14:15:23 +0900

    update transaction_processor

--- serverless/transaction_processor/lambda_function.py
+++ serverless/transaction_processor/lambda_function.py
@@ -31,7 +31,7 @@ def lambda_handler(event, context):
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-    raise e
+        raise e
 
     # Read the Transactions CSV file. Delimiter is the '|' character
     with open(localFilename) as csvfile:
