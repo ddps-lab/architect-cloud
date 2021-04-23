e190fa7e6be536cf9786954494da19ae3f23b43f
unhochoi <yms04089@kookmin.ac.kr>
2021-04-23 13:58:08 +0900

    Update start.sh

--- sample-application/flask-inference/start.sh
+++ sample-application/flask-inference/start.sh
@@ -2,7 +2,7 @@
 sudo yum update -y
 sudo yum install git -y
 sudo yum install python3-pip -y
-pip3 install virtualenv
+sudo pip3 install virtualenv
 
 cd /home/ec2-user/
 git clone https://github.com/kmu-bigdata/architect-cloud.git
@@ -12,7 +12,7 @@ mkdir -p static/uploads
 virtualenv venv
 . venv/bin/activate
 sudo -H pip3 install --upgrade --ignore-installed pip setuptools
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
 
 sudo cp main.service /etc/systemd/system/
 sudo systemctl start main
