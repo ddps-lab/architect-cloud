685acd8a7f2fa183ba48fdcf27e42109fe3b59b5
Sungjae Lee <odobenuseKR@gmail.com>
2021-05-05 20:30:58 +0900

    Update package_install.sh

--- sample-application/serverless-inference/package_install.sh
+++ sample-application/serverless-inference/package_install.sh
@@ -3,10 +3,10 @@
 # install python3.8
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install openssl-devel bzip2-devel libffi-devel
-wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
-tar xvf Python-3.8.3.tgz
-cd Python-3.8*/
-./configure --enable-optimizations
+sudo wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
+sudo tar xvf Python-3.8.3.tgz
+sudo cd Python-3.8*/
+sudo ./configure --enable-optimizations
 sudo make altinstall
 
 # install Package Setting
