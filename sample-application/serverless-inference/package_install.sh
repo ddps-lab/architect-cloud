34114c192eac8593a6cb31e5427b1a6e94b4a007
Sungjae Lee <odobenuseKR@gmail.com>
2021-05-05 20:08:26 +0900

    Create package_install.sh

---
+++ sample-application/serverless-inference/package_install.sh
@@ -0,0 +1,18 @@
+# install python3.8
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install openssl-devel bzip2-devel libffi-devel
+wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
+tar xvf Python-3.8.3.tgz
+cd Python-3.8*/
+./configure --enable-optimizations
+sudo make altinstall
+
+# install Package Setting
+sudo yum -y install gcc-c++
+sudo yum -y install python3-devel
+
+# install Packages
+pip3.8 install --upgrade --target efs/packages/ numpy
+pip3.8 install --upgrade --target efs/packages/ Pillow
+pip3.8 install --upgrade --target efs/packages/ requests-toolbelt
+pip3.8 install --upgrade --target efs/packages/ tensorflow
