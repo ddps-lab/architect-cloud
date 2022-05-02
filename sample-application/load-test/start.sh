a5f1be6745f411328e1496511a06083df6acf6fe
unhochoi <yms04089@kookmin.ac.kr>
2022-05-03 01:25:06 +0900

    Update start.sh

--- sample-application/load-test/start.sh
+++ sample-application/load-test/start.sh
@@ -3,9 +3,9 @@ sudo yum -y update
 sudo yum -y install gcc
 sudo yum -y install python3-devel
 sudo yum -y install git
-sudo pip3 install locust
-sudo pip3 install Flask==1.1.2
-sudo pip3 install Jinja2==2.11.3
+sudo pip3 install locust==2.8.6
+sudo pip3 install Flask==2.1.2
+sudo pip3 install Jinja2==3.0.0
 
 git clone https://github.com/ddps-lab/architect-cloud.git
 cd ./architect-cloud/sample-application/load-test
