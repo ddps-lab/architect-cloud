d3074e2d5256aa6bbf9c0e3c9c27ffb45a9b8c42
Cheon Sung Kyu <69793985+mumat0103@users.noreply.github.com>
2024-04-27 08:18:46 +0900

    Update index.js
    환경변수에 APP_PORT가 없을 때, 80 포트를 열도록 수정

--- 2024/monolithic_code/index.js
+++ 2024/monolithic_code/index.js
@@ -136,7 +136,7 @@ app.use(function (req, res, next) {
 });
 
 // set port, listen for requests
-const app_port = process.env.APP_PORT || 8080;
+const app_port = process.env.APP_PORT || 80;
 app.listen(app_port, () => {
 	console.log(`Server is running on port ${app_port}.`);
 });
