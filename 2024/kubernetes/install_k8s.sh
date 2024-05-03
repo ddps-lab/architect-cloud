1bdb1b777f09a6dfc21d8080fdec3a1984f1fdc8
mumat0103 <wsx2138@naver.com>
2024-05-03 12:26:45 +0900

    Add shell script for install k8s

---
+++ 2024/kubernetes/install_k8s.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#update apt & install unzip, awscli
+echo "INSTALL awscli"
+sudo apt update
+sudo apt install unzip awscli -y
+
+#install kubectl
+echo "INSTALL kubectl"
+sudo curl --silent --location -o /usr/local/bin/kubectl \
+	   https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+sudo chmod +x /usr/local/bin/kubectl
+kubectl version
+
+#install eksctl
+echo "INSTALL eksctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv -v /tmp/eksctl /usr/local/bin
+eksctl version
